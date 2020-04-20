7bbe7aa27e86ade08b613831ac5a5c4b5eae416b
rpw1 <47835461+rpw1@users.noreply.github.com>
2020-04-20 13:23:48 -0400

    second try with classifier

--- classify.py
+++ classify.py
@@ -1,40 +1,50 @@
 from picture import Picture
 from sample import MnistData
-from sampleData import SampleData
+from TrainingData import TrainingData
+from queue import ItemQueue
+import random, math
 
 class KNN:
-    
-    def classifyPicture(self, picture : Picture, label : int, samples : list) :
-        """
-        Takes in a picture and a list of list of sample pictures
-        """
-        data = MnistData()
-        lowest_distance = None
-        classification = None
-        for sample in samples:
-            new_distance = picture.getDistance(sample.grid)
-            if lowest_distance == None:
-                lowest_distance = new_distance
-                classification = sample.classification
-            elif new_distance < lowest_distance:
-                lowest_distance = new_distance
-                classification = sample.classification
-        return classification == label
+
+    k : int = None
+    white_space : int = None
+    training_data : dict = None
+
+    def __init__(self, k = 3, white_space = 50):
+        self.k = k
+        self.white_space = white_space
+        t = TrainingData(k)
+        self.training_data = t.training_data
+
+    def classify_picture(self, picture : Picture, label : int):
+        distances : ItemQueue = ItemQueue()
+        for key in self.training_data:
+            for training_picture in self.training_data[key]:
+                current_distance = picture.getDistance(self.white_space, training_picture)
+                distances.insert(current_distance, key)
+        k_distances, k_labels = distances.getMultiple(self.k)
+        guessed_label = self.getLabel(k_labels)
+        print("Guessed: " + str(guessed_label))
+        print("Actual: " + str(label))
+
+
+    def getLabel(self, labels : list):
+        max_value = -1
+        classification = -1
+        counts : dict() = dict()
+        for label in labels:
+            if label in counts:
+                continue
+            count = labels.count(label)
+            counts[label] = count
+            if count > max_value:
+                max_value = count
+                classification = label
+        return classification
         
 
-    def classifyDataset(self, list_of_grids, list_of_labels, samples):
-        length = len(list_of_grids)
-        rate = 0
-        count = 1
-        for x in range(length):
-            print(count)
-            rate = rate + self.classifyPicture(Picture(list_of_grids[x]),list_of_labels[x],samples)
-            count = count + 1
-        print(rate / length)
-
 if __name__ == "__main__":
-    nonePicture = Picture(None)
     data = MnistData()
     knn = KNN()
-    #knn.classifyPicture(Picture(data.test_images[0]), data.test_labels[0], nonePicture.getSamples())
-    #knn.classifyDataset(data.test_images, data.test_labels, nonePicture.getSamples())
+    ran_index = random.randint(0, len(data.test_images))
+    knn.classify_picture(Picture(data.test_images[ran_index]), data.test_labels[ran_index])
