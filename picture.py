ba741e65ba84110259ed2b9ca6473c1b31467907
rpw1 <47835461+rpw1@users.noreply.github.com>
2020-04-20 21:47:07 -0400

    repo cleanup

--- picture.py
+++ picture.py
@@ -2,13 +2,12 @@ import sys, math
 from sampleData import SampleData
 
 class Picture:
-    #increase threshold for point
+
     grid = None
     classification = None
 
     def __init__(self, grid):
-        if grid != None:
-            self.grid = grid
+        self.grid = grid
 
     def index_to_point(self, index : int):
         """
@@ -47,64 +46,3 @@ class Picture:
                     lowest_distance = current_distance
             distance = distance + lowest_distance
         return distance
-
-
-    # def getSamples(self) :
-    #     picture_samples = list()
-    #     s = SampleData()
-
-    #     picture0 = Picture(s.test_0)
-    #     picture0.classification = 0
-    #     picture_samples.append(picture0)
-
-    #     picture11 = Picture(s.test_1_type1)
-    #     picture11.classification = 1
-    #     picture_samples.append(picture11)
-
-    #     picture12 = Picture(s.test_1_type2)
-    #     picture12.classification = 1
-    #     picture_samples.append(picture12)
-
-    #     picture2 = Picture(s.test_2)
-    #     picture2.classification = 2
-    #     picture_samples.append(picture2)
-
-    #     picture3 = Picture(s.test_3)
-    #     picture3.classification = 3
-    #     picture_samples.append(picture3)
-
-    #     picture41 = Picture(s.test_4_type1)
-    #     picture41.classification = 4
-    #     picture_samples.append(picture41)
-
-    #     picture42 = Picture(s.test_4_type2)
-    #     picture42.classification = 4
-    #     picture_samples.append(picture42)
-
-    #     picture5 = Picture(s.test_5)
-    #     picture5.classification = 5
-    #     picture_samples.append(picture5)
-
-    #     picture6 = Picture(s.test_6)
-    #     picture6.classification = 6
-    #     picture_samples.append(picture6)
-
-    #     picture71 = Picture(s.test_7_type1)
-    #     picture71.classification = 7
-    #     picture_samples.append(picture71)
-
-    #     picture72 = Picture(s.test_7_type2)
-    #     picture72.classification = 7
-    #     picture_samples.append(picture72)
-
-    #     picture8 = Picture(s.test_8)
-    #     picture8.classification = 8
-    #     picture_samples.append(picture8)
-
-    #     picture9 = Picture(s.test_9)
-    #     picture9.classification = 9
-    #     picture_samples.append(picture9)
-
-    #     return picture_samples
-
-    
