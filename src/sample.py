f296300567de44c31d3e7fb1df28ab7c23d97be0
rpw1 <47835461+rpw1@users.noreply.github.com>
2020-04-28 17:48:50 -0400

    Added training data for + and -

--- src/sample.py
+++ src/sample.py
@@ -16,3 +16,9 @@ class MnistData:
         self.training_images, self.training_labels = self.mndata.load_training()
 
         self.test_images, self.test_labels = self.mndata.load_testing()
+
+
+if __name__ == "__main__":
+    data = MnistData()
+    print(data.test_images[0])
+    print(data.mndata.display(data.test_images[0]))
